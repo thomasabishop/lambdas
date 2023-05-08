3ffa1ad5b91d6ad82b6f37d7865f47159a8f291c
thomasabishop <tactonbishop@gmail.com>
2023-05-08 20:32:51 +0100

    feature: complete basic functionality

---
+++ src/get-summary/helpers/getApiKey.ts
@@ -0,0 +1,19 @@
+import * as AWS from 'aws-sdk'
+
+/**
+ * Retrieves the API key from the AWS Secrets Manager or if local the environment variables.
+ *
+ */
+
+const getApiKey = async (): Promise<string> => {
+    const secretsManager = new AWS.SecretsManager()
+    if (process.env.NODE_ENV === 'production') {
+        const response = await secretsManager.getSecretValue({ SecretId: process.env.SECRET_ARN as string }).promise()
+        const secretValues = JSON.parse(response.SecretString as string)
+        return secretValues.API_KEY
+    } else {
+        return process.env.API_KEY as string
+    }
+}
+
+export { getApiKey }
--- src/get-summary/index.ts
+++ src/get-summary/index.ts
@@ -1,38 +1,35 @@
-import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
-import * as AWS from 'aws-sdk';
+import { APIGatewayProxyResult } from 'aws-lambda'
+import { getApiKey } from './helpers/getApiKey'
+import { base64Encode } from './helpers/base64Encode'
+import { FetchClient } from './helpers/FetchClient'
+import { RequestInit } from 'node-fetch'
 
-const secretsManager = new AWS.SecretsManager();
-
-async function getApiKey(): Promise<string> {
-    if (process.env.NODE_ENV === 'production') {
-        const response = await secretsManager.getSecretValue({ SecretId: process.env.SECRET_ARN as string }).promise();
-        const secretValues = JSON.parse(response.SecretString as string);
-        return secretValues.API_KEY;
-    } else {
-        return process.env.API_KEY as string;
-    }
-}
-
-export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-    console.log('env var: ' + JSON.stringify(process.env));
-    let response: APIGatewayProxyResult;
+export const handler = async (): Promise<APIGatewayProxyResult> => {
+    const fetchClient = new FetchClient('https://wakatime.com/api/v1')
+    let response: APIGatewayProxyResult
     try {
-        const apiKey = await getApiKey();
+        const apiKey = await getApiKey()
+        const encodedApiKey = base64Encode(apiKey)
+        const init: RequestInit = {
+            headers: {
+                Authorization: `Basic ${encodedApiKey}`,
+            },
+        }
+        let data = await fetchClient.get('/users/current/stats', init)
+        let dataJson = (await data.json()) as string
         response = {
             statusCode: 200,
-            body: JSON.stringify({
-                message: `API KEY is: ${apiKey}`,
-            }),
-        };
+            body: dataJson,
+        }
     } catch (err: unknown) {
-        console.error(err);
+        console.error(err)
         response = {
             statusCode: 500,
             body: JSON.stringify({
                 message: err instanceof Error ? err.message : 'some error happened',
             }),
-        };
+        }
     }
 
-    return response;
-};
+    return response
+}
