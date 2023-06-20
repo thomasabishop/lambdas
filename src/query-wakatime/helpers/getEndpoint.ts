9328840b1350669aa47b6bf0dc4b3023ed2ed17c
thomasabishop <tactonbishop@gmail.com>
2023-06-20 16:45:27 +0100

    feature: add today-only Wakatime endpoint

--- src/query-wakatime/helpers/getEndpoint.ts
+++ src/query-wakatime/helpers/getEndpoint.ts
@@ -1,10 +1,12 @@
-const getEndpoint = (path: string, timePeriod: string | undefined) => {
+const getEndpoint = (path: string, timePeriod?: string | undefined) => {
     const basePath = "/users/current"
     switch (path) {
         case "/query-wakatime/main-metrics":
             return `${basePath}/stats/${timePeriod}`
         case "/query-wakatime/durations":
             return `${basePath}/summaries?range=${timePeriod}`
+        case "/query-wakatime/today-only":
+            return `${basePath}/status_bar/today`
     }
 }
 
