265bf5db09ea01a6ce40c54c0c0cf1099101587e
thomasabishop <tactonbishop@gmail.com>
2023-08-28 16:21:44 +0100

    feature (gsheet-utils): complete core functionality

--- gsheet-utils/update_sheet/app.py
+++ gsheet-utils/update_sheet/app.py
@@ -1,37 +1,37 @@
 import json
 from gspread.exceptions import SpreadsheetNotFound, APIError
 
-from helpers import client
+from helpers import auth
 from helpers import update_articles
 
 
-def lambda_handler(event, context):
+def lambda_handler(event):
     try:
         body = json.loads(event.get("body", "{}"))
-        sheet_title = body.get("sheet_title")
-        sheet_data = body.get("sheet_data")
+        worksheet = body.get("worksheet")
+        data = body.get("data")
 
         # Create list of lists from sheet_data JSON
-        entries = list(map(lambda d: list(d.values()), sheet_data))
+        entries = list(map(lambda d: list(d.values()), data))
 
         # Client for interacting with Google services:
-        sheets_client = client.create()
+        client = auth.create_client()
         try:
-            # Retrieve sheet:
-            sheet = sheets_client.open(sheet_title).sheet1
+            # Retrieve worksheet:
+            worksheet = client.open(worksheet).sheet1
 
         except SpreadsheetNotFound:
             return {
                 "statusCode": 404,
-                "body": json.dumps({"message": f"Sheet {sheet} not found."}),
+                "body": json.dumps({"message": f"Sheet {worksheet} not found."}),
             }
 
         # Attempt data insertion:
-        update_articles.add_entries_to_sheet(sheet, entries)
+        update_articles.add_entries_to_sheet(worksheet, entries)
 
         return {
             "statusCode": 200,
-            "body": json.dumps({"message": f"Sheet {sheet} updated successfully"}),
+            "body": json.dumps({"message": f"Sheet {worksheet} updated successfully"}),
         }
 
     except APIError as e:
