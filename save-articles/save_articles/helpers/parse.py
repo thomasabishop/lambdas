258a93c67d5be47eaf91aeef541fff47bebd8397
thomasabishop <tactonbishop@gmail.com>
2023-09-04 15:16:02 +0100

    refactor (save-articles): add timestamp property, remove excerpt

--- save-articles/save_articles/helpers/parse.py
+++ save-articles/save_articles/helpers/parse.py
@@ -4,16 +4,16 @@
 def articles(articles_dict):
     result = []
     for value in articles_dict.values():
+        timestamp = value.get("time_added", "undefined")
         date_added = value.get("time_added", "undefined")
         article_title = value.get("given_title", "undefined")
-        article_excerpt = value.get("excerpt", "undefined")
         link = value.get("resolved_url", "undefined")
 
         result.append(
             {
+                "timestamp": timestamp,
                 "date_added": date_added,
                 "article_title": article_title,
-                "article_excerpt": article_excerpt,
                 link: link,
             }
         )
