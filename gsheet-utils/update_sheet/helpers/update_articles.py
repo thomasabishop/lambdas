b16a46532b7623d91c971b6d5903c4ab69eed57e
thomasabishop <tactonbishop@gmail.com>
2023-09-04 15:13:46 +0100

    refactor (gsheet-utils): rm redundant functions

--- gsheet-utils/update_sheet/helpers/update_articles.py
+++ gsheet-utils/update_sheet/helpers/update_articles.py
@@ -3,24 +3,10 @@ import datetime
 """Utils for updating a list of article entries in Google sheets"""
 
 
-# TODO: removal of dupes not working
-
-
 def remove_duplicate_entries(entries, unique_key_index):
     unique_dict = {x[unique_key_index]: x for x in entries}
     unique_entries = list(unique_dict.values())
     return unique_entries
-
-
-def update_entries(old, new):
-    """Merge old and new data, removing dupes"""
-    old_unix_date = list(map(lambda i: [date_to_timestamp(i[0])] + i[1:], old))
-    combined = old_unix_date + new
-    deduped = remove_duplicate_entries(combined, 0)
-    # seen = set()
-    # filter_func = lambda x: not (tuple(x) in seen or seen.add(tuple(x)))
-    # unique_combined = list(filter(filter_func, combined))
-    return deduped
 
 
 def sort_by_date(entries, date_index=0):
@@ -40,21 +26,13 @@ def timestamp_to_date(timestamp):
     return formatted_date
 
 
-def date_to_timestamp(date_str):
-    """Convert human readable date to Unix timestamp"""
-
-    date_object = datetime.datetime.strptime(date_str, "%d-%m-%Y")
-    timestamp = int(date_object.timestamp())
-
-    return timestamp
-
-
 def add_entries_to_sheet(sheet_ref, new_data):
     """Write updated data to sheet"""
     existing_entries = sheet_ref.get_all_values()
-    entries_rows = existing_entries[0:]
-    updated = update_entries(entries_rows, new_data)
-    date_sorted = sort_by_date(updated)
+    merged_entries = existing_entries + new_data
+    deduped = remove_duplicate_entries(merged_entries, 1)
+    date_sorted = sort_by_date(deduped)
+    # learning exercise: convert this map into list comprehension
     readable_date = list(map(lambda i: [timestamp_to_date(i[0])] + i[1:], date_sorted))
     sheet_ref.clear()
     sheet_ref.insert_rows(readable_date, 1)
