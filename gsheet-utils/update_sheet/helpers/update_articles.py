347396b2df606ca65e81b7d2832420f65224d36c
thomasabishop <tactonbishop@gmail.com>
2023-09-05 20:25:01 +0100

    refactor (gsheet-utils): use better naming for helper funcs

--- gsheet-utils/update_sheet/helpers/update_articles.py
+++ gsheet-utils/update_sheet/helpers/update_articles.py
@@ -3,19 +3,19 @@ import datetime
 """Utils for updating a list of article entries in Google sheets"""
 
 
-def remove_duplicate_entries(entries, unique_key_index):
-    unique_dict = {x[unique_key_index]: x for x in entries}
+def remove_duplicates(entries, unique_key):
+    unique_dict = {x[unique_key]: x for x in entries}
     unique_entries = list(unique_dict.values())
     return unique_entries
 
 
-def sort_by_date(entries, date_index=0):
+def date_sort(entries, date_index=0):
     """Sort data by descending date"""
     entries.sort(key=lambda x: int(x[date_index]), reverse=True)
     return entries
 
 
-def timestamp_to_date(timestamp):
+def convert_timestamp(timestamp):
     """Convert Unix timestamp to human readable date"""
 
     if isinstance(timestamp, str):
@@ -27,11 +27,11 @@ def timestamp_to_date(timestamp):
 
 
 def collate_entries(sheet_ref, new_entries):
-    processed_entries = sort_by_date(
-        remove_duplicate_entries(sheet_ref.get_all_values() + new_entries, 1)
+    processed_entries = date_sort(
+        remove_duplicates(sheet_ref.get_all_values() + new_entries, 1)
     )
 
-    return [[timestamp_to_date(i[0])] + i[1:] for i in processed_entries]
+    return [[convert_timestamp(i[0])] + i[1:] for i in processed_entries]
 
     # existing_entries = sheet_ref.get_all_values()
     # merged_entries = existing_entries + new_data
