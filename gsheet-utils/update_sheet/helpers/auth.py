4a93a78a3a04f6cda6cbe39231141fdf65fd5c6c
thomasabishop <tactonbishop@gmail.com>
2023-09-07 19:29:01 +0100

    refactor (gsheet-utils): remove secrets manager dependency

--- gsheet-utils/update_sheet/helpers/auth.py
+++ gsheet-utils/update_sheet/helpers/auth.py
@@ -4,21 +4,7 @@ Authenticate access to Google services
 import os
 import json
 import gspread
-import boto3
 from oauth2client.service_account import ServiceAccountCredentials
-
-
-def get_secret():
-    python_env = os.environ.get("PYTHON_ENV", "local")
-    """In production, source creds from SecretsManager"""
-    if python_env == "production":
-        secrets_manager = boto3.client("secretsmanager")
-        response = secrets_manager.get_secret_value(SecretId=os.environ["SECRET_ARN"])
-        secret_values = json.loads(response["SecretString"])
-        return secret_values["GOOGLE_CREDS"]
-    else:
-        """If local, source creds from env var"""
-        return json.loads(os.environ["GOOGLE_CREDS"])
 
 
 def get_credentials():
@@ -28,7 +14,8 @@ def get_credentials():
         "https://www.googleapis.com/auth/drive",
     ]
 
-    creds_json = get_secret()
+    # creds_json = get_secret()
+    creds_json = json.loads(os.environ["GOOGLE_CREDS"])
     creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_json, scope)
 
     return creds
