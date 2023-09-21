51ca112b5e43b3ece08154e81da6c222e7a4b548
thomasabishop <tactonbishop@gmail.com>
2023-09-21 21:03:15 +0100

    test (pocket-api): get basic handler test working

--- pocket-api/query-pocket/tests/unit/test-handler.test.ts
+++ pocket-api/query-pocket/tests/unit/test-handler.test.ts
@@ -1,65 +1,38 @@
-import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
-import { lambdaHandler } from '../../../../hello-world/app';
-import { expect, describe, it } from '@jest/globals';
+/// <reference types="@types/jest" />;
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda"
+import { handler } from "../../index"
+import { expect, describe, it } from "@jest/globals"
+import { event } from "../fixtures/event"
+import axios from "axios"
 
-describe('Unit test for app handler', function () {
-    it('verifies successful response', async () => {
-        const event: APIGatewayProxyEvent = {
-            httpMethod: 'get',
-            body: '',
+jest.mock("axios")
+const mockedAxios = axios as jest.Mocked<typeof axios>
+
+jest.mock("../../helpers/getPocketCredentials", () => ({
+    getPocketCredentials: jest.fn().mockReturnValue({
+        accessToken: "mockAccessToken",
+        consumerKey: "mockConsumerKey",
+    }),
+}))
+
+describe("handler", () => {
+    it("verifies successful response", async () => {
+        ;(mockedAxios.post as jest.Mock).mockResolvedValue({
+            data: {},
+            status: 200,
+            statusText: "OK",
             headers: {},
-            isBase64Encoded: false,
-            multiValueHeaders: {},
-            multiValueQueryStringParameters: {},
-            path: '/hello',
-            pathParameters: {},
-            queryStringParameters: {},
-            requestContext: {
-                accountId: '123456789012',
-                apiId: '1234',
-                authorizer: {},
-                httpMethod: 'get',
-                identity: {
-                    accessKey: '',
-                    accountId: '',
-                    apiKey: '',
-                    apiKeyId: '',
-                    caller: '',
-                    clientCert: {
-                        clientCertPem: '',
-                        issuerDN: '',
-                        serialNumber: '',
-                        subjectDN: '',
-                        validity: { notAfter: '', notBefore: '' },
-                    },
-                    cognitoAuthenticationProvider: '',
-                    cognitoAuthenticationType: '',
-                    cognitoIdentityId: '',
-                    cognitoIdentityPoolId: '',
-                    principalOrgId: '',
-                    sourceIp: '',
-                    user: '',
-                    userAgent: '',
-                    userArn: '',
-                },
-                path: '/hello',
-                protocol: 'HTTP/1.1',
-                requestId: 'c6af9ac6-7b61-11e6-9a41-93e8deadbeef',
-                requestTimeEpoch: 1428582896000,
-                resourceId: '123456',
-                resourcePath: '/hello',
-                stage: 'dev',
-            },
-            resource: '',
-            stageVariables: {},
-        };
-        const result: APIGatewayProxyResult = await lambdaHandler(event);
+            config: {},
+        })
 
-        expect(result.statusCode).toEqual(200);
+        const result: APIGatewayProxyResult = await handler(event)
+
+        expect(result.statusCode).toEqual(200)
+
         expect(result.body).toEqual(
             JSON.stringify({
-                message: 'hello world',
+                data: {},
             }),
-        );
-    });
-});
+        )
+    })
+})
