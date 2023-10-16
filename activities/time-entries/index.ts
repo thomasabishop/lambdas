d5c00f6975bcf1c8902ade789c275ee9d8ee5a50
thomasabishop <tactonbishop@gmail.com>
2023-10-16 07:23:37 +0100

    fix (activities): resolve conflict

--- activities/time-entries/index.ts
+++ activities/time-entries/index.ts
@@ -1,7 +1,14 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda"
+import { TogglClient } from "./helpers/TogglClient"
+import { getProjects } from "./helpers/getProjects"
 
 export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     try {
+        // const togglClient = new TogglClient()
+        // const testData = await togglClient.get("me/time_entries?start_date=2023-09-01&end_date=2023-09-22")
+        // const projects = await getProjects()
+        // const dummyProject = 193325926
+        // const example = projects[dummyProject]
         return {
             statusCode: 200,
             body: JSON.stringify({
