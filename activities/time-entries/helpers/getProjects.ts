70668120877b14e5165014b99e55b87a3e35968b
thomasabishop <tactonbishop@gmail.com>
2023-10-17 17:39:56 +0100

    feature (activities): add getProjects function and test

---
+++ activities/time-entries/helpers/getProjects.ts
@@ -0,0 +1,25 @@
+import { TogglClient } from "./TogglClient"
+
+interface IProject {
+    id: number
+    name: string
+    [key: string]: unknown
+}
+
+type TProjectMap = Record<IProject["id"], IProject["name"]>
+
+const getProjects = async (): Promise<TProjectMap> => {
+    const workspace = process.env.TOGGL_WORKSPACE_ID
+    const togglClient = new TogglClient()
+    const projects: IProject[] = await togglClient.get(`workspaces/${workspace}/projects`)
+    return parseProjects(projects)
+}
+
+const parseProjects = (projects: IProject[]): TProjectMap => {
+    return projects.reduce((projectMap: TProjectMap, project: IProject) => {
+        projectMap[project.id] = project.name
+        return projectMap
+    }, {})
+}
+
+export { getProjects, parseProjects }
---
+++ activities/time-entries/tests/unit/getProjects.test.ts
@@ -0,0 +1,34 @@
+import { expect, describe, it } from "@jest/globals"
+import rawProjectData from "../fixtures/toggl_raw_projects.json"
+import { getProjects } from "../../helpers/getProjects"
+
+jest.mock("../../helpers/TogglClient", () => {
+    return {
+        TogglClient: jest.fn().mockImplementation(() => {
+            return {
+                get: jest.fn().mockResolvedValue(rawProjectData),
+            }
+        }),
+    }
+})
+
+describe("getProjects", () => {
+    const parsedProjects = {
+        "193325937": "Blogging",
+        "193325940": "Projects",
+        "193326477": "Practical study",
+        "193325926": "Code exercises",
+        "193325907": "Technical articles and podcasts",
+        "193325932": "Theoretical study",
+    }
+
+    it("should return a map of project IDs to project names", async () => {
+        const result = await getProjects()
+        expect(result).toEqual(parsedProjects)
+    })
+
+    it("should be possible to individuate a project name by the project ID", async () => {
+        const result = await getProjects()
+        expect(result[193325937]).toEqual("Blogging")
+    })
+})
