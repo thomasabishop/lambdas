0cdbfaf9a3ed4de773e1368ecdf16baf89aa5877
thomasabishop <tactonbishop@gmail.com>
2023-10-22 19:30:44 +0100

    feature (activities): complete restructuring of handler

--- activities/time-entries/index.ts
+++ activities/time-entries/index.ts
@@ -1,26 +1,40 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda"
 import { TogglClient } from "./helpers/TogglClient"
 import { getProjects } from "./helpers/getProjects"
+import { getTimeEntries } from "./helpers/getTimeEntries"
+import { DateRange } from "./helpers/getDateRange"
+import { validateDateRange } from "./helpers/validateDateRange"
+const togglClient = new TogglClient()
+const workspaceId = process.env.TOGGL_WORKSPACE_ID as string
 
 export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     try {
-        // const togglClient = new TogglClient()
-        // const testData = await togglClient.get("me/time_entries?start_date=2023-09-01&end_date=2023-09-22")
-        // const projects = await getProjects()
-        // const dummyProject = 193325926
-        // const example = projects[dummyProject]
+        const projects = await getProjects(workspaceId, togglClient)
+        const dateRange = validateDateRange(event.queryStringParameters?.date_range) || DateRange.Month
+
+        const timeEntries = await getTimeEntries(dateRange, togglClient, projects)
+
         return {
             statusCode: 200,
             body: JSON.stringify({
-                message: "hello world",
+                data: timeEntries,
+                message: "Successfully retrieved time entries",
             }),
         }
-    } catch (err) {
-        console.log(err)
+    } catch (error: unknown) {
+        console.log(error)
+        if (error instanceof Error && error.message.includes("No data for the selected date range")) {
+            return {
+                statusCode: 400,
+                body: JSON.stringify({
+                    message: error.message,
+                }),
+            }
+        }
         return {
             statusCode: 500,
             body: JSON.stringify({
-                message: "some error happened",
+                message: error,
             }),
         }
     }
