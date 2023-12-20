42ca95799090c61a530f2b2990711581dbc9f9f0
thomasabishop <tactonbishop@gmail.com>
2023-12-20 17:33:59 +0000

    feature (activities): reverse sort resulting time entries

--- node-js/activities/src/lib/getTimeEntries.ts
+++ node-js/activities/src/lib/getTimeEntries.ts
@@ -1,10 +1,9 @@
 import { TProjectMap } from "./getProjects"
 import { TogglClient } from "./TogglClient"
 import { getDateRange } from "./getDateRange"
-import { formatDate } from "./formatDate"
 import { formatTime } from "./formatTime"
 import { DateRange } from "./getDateRange"
-
+import { formatLongDate } from "./formatLongDate"
 interface ITogglTimeEntry {
     project_id: number
     at: string
@@ -29,12 +28,14 @@ const getTimeEntries = async (
         const timeEntries: ITogglTimeEntry[] = await togglClient.get(`me/time_entries?${formattedDateRange}`)
 
         if (timeEntries) {
-            return timeEntries.map((entry: ITogglTimeEntry) => ({
-                project: projects[entry.project_id],
-                date: formatDate(entry.at),
-                duration: formatTime(entry.duration),
-                task_description: entry.description,
-            }))
+            return timeEntries
+                .map((entry: ITogglTimeEntry) => ({
+                    project: projects[entry.project_id],
+                    date: formatLongDate(entry.at),
+                    duration: formatTime(entry.duration),
+                    task_description: entry.description,
+                }))
+                .reverse()
         }
     } catch (error: unknown) {
         console.error(error)
