0ea23079373c6e09184c33f086bc456425d8d7b5
thomasabishop <tactonbishop@gmail.com>
2024-06-15 09:11:13 +0100

    chore: delete old exports and schemas

--- node-js/time-tracking/schema/time_entiries_table_sql.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE time_entries (
-        id INT AUTO_INCREMENT PRIMARY KEY,
-        activity_type VARCHAR(255) NOT NULL,
-        \`start\` DATETIME NOT NULL,
-        \`end\` DATETIME NOT NULL,
-        duration DECIMAL(10, 2),
-        description VARCHAR(255),
-        UNIQUE KEY unique_time_entry (activity_type, \`start\`, \`end\`)
-    )
