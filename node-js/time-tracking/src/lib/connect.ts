18ff5ba6d677b7232c7ebd0683200f1c597a443f
thomasabishop <tactonbishop@gmail.com>
2024-06-19 17:17:14 +0100

    refactor: remove endpoint in prod

--- node-js/time-tracking/src/lib/connect.ts
+++ node-js/time-tracking/src/lib/connect.ts
@@ -5,7 +5,7 @@ const isLocal = AWS_SAM_LOCAL === "true"
 
 const client = new DynamoDBClient({
    region: isLocal ? "localhost" : "eu-west-2",
-   endpoint: DB_ENDPOINT,
+   endpoint: isLocal ? DB_ENDPOINT : "",
    credentials: {
       accessKeyId: ACCESS_KEY as string,
       secretAccessKey: SECRET_KEY as string,
