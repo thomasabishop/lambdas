d47b81b494bf322762df3a582e3c07df3a26da18
thomasabishop <tactonbishop@gmail.com>
2024-06-19 19:19:06 +0100

    fix: suppress endpoint in prod db connection

--- node-js/time-tracking/src/lib/connect.ts
+++ node-js/time-tracking/src/lib/connect.ts
@@ -3,13 +3,30 @@ import { DynamoDBClient } from "@aws-sdk/client-dynamodb"
 const { AWS_SAM_LOCAL, DB_ENDPOINT, ACCESS_KEY, SECRET_KEY } = process.env
 const isLocal = AWS_SAM_LOCAL === "true"
 
-const client = new DynamoDBClient({
-   region: isLocal ? "localhost" : "eu-west-2",
-   endpoint: isLocal ? DB_ENDPOINT : "",
-   credentials: {
-      accessKeyId: ACCESS_KEY as string,
-      secretAccessKey: SECRET_KEY as string,
-   },
-})
+const credentials = {
+   accessKeyId: ACCESS_KEY as string,
+   secretAccessKey: SECRET_KEY as string,
+}
+
+const localParams = {
+   region: "localhost",
+   endpoint: DB_ENDPOINT,
+   credentials: credentials,
+}
+
+const prodParams = {
+   region: "eu-west-2",
+   credentials: credentials,
+}
+
+const client = new DynamoDBClient(isLocal ? localParams : prodParams)
+
+// const client = new DynamoDBClient({
+//    region: isLocal ? "localhost" : "eu-west-2",
+//    credentials: {
+//       accessKeyId: ACCESS_KEY as string,
+//       secretAccessKey: SECRET_KEY as string,
+//    },
+// })
 
 export { client }
