b742f5ebf14f8f8c95264bbd1c28ee6622404b0f
thomasabishop <tactonbishop@gmail.com>
2024-06-30 18:05:27 +0100

    scripts: handle remote upload of weekly data and handle messages for entries greater than length 10

--- node-js/time-tracking/scripts/upload_daily_entries.py
+++ node-js/time-tracking/scripts/upload_daily_entries.py
@@ -1,10 +1,13 @@
-# Upload daily time-entries export from Timewarrior to remote DB
-
 #! /usr/local/bin/python3
+
+# Upload daily time-entries export from Timewarrior to remote DB
+# E.g: ENDPOINT=http://example.com python3 ./upload_daily_entries.py today|week
+
 import subprocess
 import requests
 import json
 import os
+import sys
 
 LAMBDA_ENDPOINT = os.getenv("ENDPOINT")
 SLACK_NOTIFIER_SCRIPT = "/home/thomas/repos/slack-notifier/src/index.js"
@@ -22,7 +25,7 @@ def slack_blocks(summary, entries):
                 }
             )
 
-    return [
+    blocks = [
         {
             "type": "section",
             "text": {"type": "mrkdwn", "text": f"*{summary}*"},
@@ -30,6 +33,12 @@ def slack_blocks(summary, entries):
         {"type": "divider"},
         {"type": "section", "fields": entry_blocks},
     ]
+    if len(entries) > 10:
+        blocks.append(
+            {"type": "section", "text": {"type": "mrkdwn", "text": "And several more."}}
+        )
+
+    return blocks
 
 
 def post(data, endpoint):
@@ -37,6 +46,7 @@ def post(data, endpoint):
         response = requests.post(endpoint, json=data)
         response.raise_for_status()
     except requests.exceptions.HTTPError as err_http:
+        print(response.json())
         return f"HTTP Error: {err_http}"
     except requests.exceptions.Timeout as err_timeout:
         return f"Timeout Error: {err_timeout}"
@@ -48,9 +58,10 @@ def post(data, endpoint):
 
 
 if __name__ == "__main__":
+    period = sys.argv[1] if len(sys.argv) > 1 else "today"
     try:
         daily_entries = subprocess.run(
-            ["python", "./export_timewarrior_entries.py", "today"],
+            ["python", "./export_timewarrior_entries.py", period],
             capture_output=True,
         )
         if daily_entries.stdout:
