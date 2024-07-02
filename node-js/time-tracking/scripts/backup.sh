f3e90c53146b430e3362c14c865cfe04c021dee3
thomasabishop <tactonbishop@gmail.com>
2024-07-02 07:04:53 +0100

    scripts: tidy backup script

--- node-js/time-tracking/scripts/backup.sh
+++ node-js/time-tracking/scripts/backup.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
 SOURCE_PROFILE="timetracking_dev"
-DEST_PROFILE="timetracking_dev"
 TABLE="TimeEntries"
-DEST_TABLE="TimeEntries"  # Make sure this is correct
 PAGE_SIZE=500
 MAX_ITEMS=10000
 
 DATE="$(date +"%Y-%m-%dT%H:%M:%SZ")"
 FILENAME="TimeEntries_bak_${DATE}.json"
+BACKUP_DIR="/home/thomas/repos/lambdas/node-js/time-tracking/backups"
 
 aws dynamodb scan \
 	--table-name "$TABLE" \
@@ -17,6 +16,6 @@ aws dynamodb scan \
   --page-size "$PAGE_SIZE" \
   --max-items "$MAX_ITEMS" \
   --output json \
-	--profile "$SOURCE_PROFILE" > ../backups/"$FILENAME"
+	--profile "$SOURCE_PROFILE" > "$BACKUP_DIR/$FILENAME"
 
 
